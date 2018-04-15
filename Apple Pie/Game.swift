c6fe43ebd252cdc1ad6e695d6f4466909dc4fc02
yellowtiramisu <croissant@protonmail.com>
2018-04-15 15:48:08 +0300

    Initial Commit

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,36 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Lorenzo G. on 2018-04-15.
+//  Copyright © 2018 Rose Software. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+    
+    // method which takes the letter pressed, adds to the collection of
+    // guessedLetters, and updates incorrectMovesRemaining
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+}
---
+++ Apple Pie/ViewController.swift
@@ -0,0 +1,106 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by Lorenzo G. on 2018-04-15.
+//  Copyright © 2018 Rose Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var listOfWords = ["apple", "lorenzo", "gulsel", "home"]
+    let incorrectMovesAllowed = 7
+    var totalWins = 0 {
+        didSet { // property observers: whenever these are updated, new round!
+            newRound()
+        }
+    }
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        newRound()
+    }
+
+    var currentGame: Game!
+    
+    func enableLetterButtons(_ enable: Bool) {
+        for button in letterButtons {
+            button.isEnabled = enable // whatever the bool says, that's what the buttons do
+        }
+    }
+    
+    func newRound() {
+        // function that runs whenever a new round starts: inits things around
+        // and creates a new instance of Game
+        
+        if !listOfWords.isEmpty {
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
+            enableLetterButtons(true)
+            updateUI()
+        } else {
+            enableLetterButtons(false) // we disable the keyboard for good.
+        }
+    }
+    
+    func updateUI() {
+        // function which updates the screen with updated game information
+        var letters = [String]()
+        for letter in currentGame.formattedWord {
+            letters.append(String(letter))
+        }
+        
+        let wordWithSpacing = letters.joined(separator: " ")
+        
+        correctWordLabel.text = wordWithSpacing
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+        
+    }
+    
+    // this function updates losses, wins, or just lets the player go on
+    func updateGameState() {
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        } else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        } else {
+            updateUI()
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        // we need UIButton as sender so that we can access its
+        // title property, something the Any type does not have.
+        // we need access to it to determine which button is to
+        // be disabled
+        sender.isEnabled = false
+        
+        // getting letter from button pressed and normalising it
+        let letterString = sender.title(for: .normal)!
+        let letter = Character(letterString.lowercased())
+        
+        // feeding that letter into the playerGuessed function
+        currentGame.playerGuessed(letter: letter)
+        updateGameState()
+    }
+
+}
+
